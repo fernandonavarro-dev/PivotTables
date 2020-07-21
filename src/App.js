beb0c0de0f169e6033c4a0f2242584318d22cf82
Fernando Navarro <fernando@coreddd.com>
2020-07-21 13:51:13 -0700

    App.js displaying with local data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+          </button>
+            <a href="index.html">Wellstar Order App</a>
+          </div>
+          <div className="header-links" >
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Product Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+          <ul>
+            <li>
+              <a href="index.html">Shirt</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main" >
+          <div className="content" >
+            {/* <Route path="/" exact={true} component={HomeScreen} /> */}
+            <ul className="products">
+              {
+                data.products.map(product =>
+                  <li>
+                    <div className="product">
+                      <img className="product-image" src={product.image} alt="productImage" />
+                      <div className="product-name">
+                        <a href="product.html">{product.name} </a>
+                      </div>
+                      <div className="product-price" >${product.price}</div>
+                    </div>
+                  </li>
+                )
+              }
+            </ul>
+          </div>
+        </main>
+        <footer className="footer">By Core ddd with React</footer>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
