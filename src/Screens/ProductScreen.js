3b73535d7355b5ad961f2e9b426c599d777a5a4d
Fernando Navarro <fernando@coreddd.com>
2020-07-26 17:21:42 -0700

    ProductScreen dispatch implementation

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -1,53 +1,61 @@
-import React from 'react';
-// import React, { useState, useEffect } from 'react';
+// import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
 
-class ProductScreen extends React.Component {
+function ProductScreen(props) {
 
-    state = {
-        fetchedProducts: [], pageProduct: []
-    }
-
-    async componentDidMount() {
-        const productRes = await axios({
-            method: 'GET',
-            url: 'http://164.90.158.158/products'
-        })
-
-        const fetchedProducts = productRes.data
-
-        const id = this.props.match.params.id
-        const idInt = parseInt(id)
-
-        const product = fetchedProducts.find(x => x['id'] === idInt);
-
-        const pageProduct = product
+    const productDetails = useSelector(state => state.productDetails);
+    console.log("productDetails, ", productDetails);
+    // const [pageProduct, setpageProduct] = useState({});
+    const dispatch = useDispatch();
+    const { products, loading, error } = productDetails;
 
 
-        this.setState({ fetchedProducts, pageProduct })
-    }
+    useEffect(() => {
+        dispatch(detailsProduct(props.match.params.id));
+        // setpageProduct(products);
+        return () => {
+            //
+        };
+    }, [])
 
-    render() {
 
-        const { pageProduct } = this.state
+    const product = { ...products }
+    delete product.thumbnail
+    // console.log("products,", products);
+    // console.log("product,", product);
+    // console.log("product.id,", product.id);
 
-        return (
-            <div>
-                <div className="back-to-result">
-                    <Link to="/">Back to result</Link>
-                </div>
-                <>
-                    <div className="details">
-                        <div className="details-image">
-                            <img src={pageProduct.imageURL} alt="product"></img>
-                        </div>
-                        <div className="details-info">
-                            <ul>
-                                <li>
-                                    <h4>{pageProduct.name}</h4>
-                                </li>
-                                {/* <li>
+    // console.log("props.match.params.id, ", props.match.params.id);
+    // console.log(Object.keys(product))
+
+    // console.log("pageProduct, ", pageProduct);
+    // console.log("products.name, ", id);
+
+    // const { ['id']: productId } = products;
+    // console.log("id, ", productId);
+
+    return (
+        <div>
+            <div className="back-to-result">
+                <Link to="/">Back to result</Link>
+            </div>
+            {loading ? <div>Loading...</div> :
+                error ? <div>{error}</div> :
+                    (
+                        <div className="details">
+                            <div className="details-image">
+                                <img src={product.imageURL} alt="product"></img>
+                            </div>
+                            <div className="details-info">
+                                <ul>
+                                    <li>
+                                        <h4>{product.name}</h4>
+                                    </li>
+                                    {/* <li>
                                 <a href="#reviews">
                                     <Rating
                                     value={product.rating}
@@ -55,50 +63,78 @@ class ProductScreen extends React.Component {
                                 />
                                 </a>
                             </li> */}
-                                <li>
-                                    Price: <b>${pageProduct.price}</b>
-                                </li>
-                                <li>
-                                    Commission: <b>%{pageProduct.commissionPercentage}</b>
-                                </li>
-                                <li>
-                                    Description:
-                  <div>{pageProduct.description}</div>
-                                </li>
-                            </ul>
+                                    <li>
+                                        Price: <b>${product.price}</b>
+                                    </li>
+                                    <li>
+                                        Commission: <b>%{product.commissionPercentage}</b>
+                                    </li>
+                                    <li>
+                                        Description:
+                  <div>{product.description}</div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="details-action">
+                                <ul>
+                                    <li>
+                                        Price: $ {product.price}
+                                    </li>
+                                    <li>
+                                        Status: {product.status}
+                                    </li>
+                                    <li>
+                                        Qty: <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                    </li>
+                                    <li>
+                                        <button className="button primary" >Add to order</button>
+                                    </li>
+                                </ul>
+                            </div>
+
                         </div>
-                        <div className="details-action">
-                            <ul>
-                                <li>
-                                    Price: $ {pageProduct.price}
-                                </li>
-                                <li>
-                                    Status: {pageProduct.status}
-                                </li>
-                                <li>
-                                    Qty: <select>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                    </select>
-                                </li>
-                                <li>
-                                    <button className="button primary" >Add to order</button>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
-                </>
-
-            </div>
-        )
-    }
-
-
+                    )
+            }
+        </div>
+    )
 }
 
+export default ProductScreen;
+
+
+// class ProductScreen extends React.Component {
+
+//     state = {
+//         fetchedProducts: [], pageProduct: []
+//     }
+
+//     async componentDidMount() {
+//         const productRes = await axios({
+//             method: 'GET',
+//             url: 'http://164.90.158.158/products'
+//         })
+
+//         const fetchedProducts = productRes.data
+
+//         const id = this.props.match.params.id
+//         const idInt = parseInt(id)
+
+//         const product = fetchedProducts.find(x => x['id'] === idInt);
+
+//         const pageProduct = product
+
+
+//         this.setState({ fetchedProducts, pageProduct })
+//     }
+
+//     render() {
+
+//         const { pageProduct } = this.state
 // console.log("this.props, ", this.props);
 
 // const id = this.props.match.params.id
@@ -222,4 +258,4 @@ class ProductScreen extends React.Component {
 //     )
 // }
 
-export default ProductScreen;
+// export default ProductScreen;
