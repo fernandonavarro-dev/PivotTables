9bc8647e5c6fca12faa1a9e771a761974a6178bb
Arslan Ahmed Zafar <gorayaa66@gmail.com>
2020-08-29 21:04:51 +0500

    Counts resolved

--- src/Screens/PTableStockCountsScreen.js
+++ src/Screens/PTableStockCountsScreen.js
@@ -1,57 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import PivotTableUI from 'react-pivottable/PivotTableUI';
-import 'react-pivottable/pivottable.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { listStockCounts } from '../actions/stockeventActions';
+import React, { useEffect, useState } from 'react'
+import PivotTableUI from 'react-pivottable/PivotTableUI'
+import 'react-pivottable/pivottable.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { listStockCounts, listStockevents } from '../actions/stockeventActions'
 
-const PTableStockCounts = (props) => {
-    const [initialState, setInitialState] = useState(props);
+const PTableStockCounts = props => {
+  const [initialState, setInitialState] = useState(props)
 
-    const userLogin = useSelector(state => state.userLogin);
-    const { userInfo } = userLogin;
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
 
-    const stockCountList = useSelector(state => state.stockCountList);
-    const { loading, plazasStocks, error } = stockCountList;
+  const stockCountList = useSelector(state => state.stockCountList)
+  const { loading, plazasStocks, error } = stockCountList
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-    console.log("plazasStocks in PTableScreen, ", plazasStocks);
+  const stockeventList = useSelector(state => state.stockeventList)
+  const { stockevents } = stockeventList
 
-    useEffect(() => {
-        if (userInfo && userInfo.user.isTop) {
-            dispatch(listStockCounts());
-        }
-        return () => {
-            //
-        };
-    }, [dispatch, userInfo]);
+  console.log('stock event is : ', stockevents)
+  console.log('plazasStocks in PTableScreen, ', plazasStocks)
 
-    return (
-        <div>
-            {loading ? (
-                <div>Loading...</div>
-            ) : error ? (
-                <div>{error}</div>
-            ) : (
-                        <>
-                            <div className="content content-margined">
-                                <h3>Pivot Table Stock Counts</h3>
-                            </div>
-                            {userInfo && userInfo.user.isTop && (
+  useEffect(() => {
+    if (userInfo && userInfo.user.isTop) {
+      dispatch(listStockCounts())
+      dispatch(listStockevents())
+    }
+    return () => {
+      //
+    }
+  }, [dispatch, userInfo])
 
-
-                                <PivotTableUI
-                                    data={plazasStocks}
-                                    onChange={s => setInitialState(s)}
-                                    {...initialState}
-                                />
-                            )}
-
-                        </>
-                    )}
-        </div>
-    );
-
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <>
+          <div className='content content-margined'>
+            <h3>Pivot Table Stock Counts</h3>
+          </div>
+          {userInfo && userInfo.user.isTop && (
+            <PivotTableUI
+              data={plazasStocks.map((plaza, index) => {
+                return { ...plaza, id: index }
+              })}
+              onChange={s => setInitialState(s)}
+              {...initialState}
+            />
+          )}
+        </>
+      )}
+    </div>
+  )
 }
 
-export default PTableStockCounts;
+export default PTableStockCounts
--- src/Screens/PTableStockEvents.js
+++ src/Screens/PTableStockEvents.js
@@ -1,62 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import PivotTableUI from 'react-pivottable/PivotTableUI';
-import 'react-pivottable/pivottable.css';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react'
+import PivotTableUI from 'react-pivottable/PivotTableUI'
+import 'react-pivottable/pivottable.css'
+import { useDispatch, useSelector } from 'react-redux'
 // import { listOrders } from '../actions/orderActions';
-import { listStockevents } from '../actions/stockeventActions';
+import { listStockevents } from '../actions/stockeventActions'
 
-const PTableStockEvents = (props) => {
+const PTableStockEvents = props => {
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
 
-    const userLogin = useSelector(state => state.userLogin);
-    const { userInfo } = userLogin;
+  // const orderList = useSelector(state => state.orderList);
+  // const { loading, orders, error } = orderList;
 
-    // const orderList = useSelector(state => state.orderList);
-    // const { loading, orders, error } = orderList;
+  const stockeventList = useSelector(state => state.stockeventList)
+  const { loading, stockevents, error } = stockeventList
 
-    const stockeventList = useSelector(state => state.stockeventList);
-    const { loading, stockevents, error } = stockeventList;
+  console.log('stock event is : ', stockevents)
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-    const [initialState, setInitialState] = useState(props)
+  const [initialState, setInitialState] = useState(props)
 
-    useEffect(() => {
-        if (userInfo && userInfo.user.isTop) {
-            // dispatch(listOrders());
-            dispatch(listStockevents());
+  useEffect(() => {
+    if (userInfo && userInfo.user.isTop) {
+      // dispatch(listOrders());
+      dispatch(listStockevents())
+    }
+    return () => {
+      //
+    }
+  }, [dispatch, userInfo])
 
-        }
-        return () => {
-            //
-        };
-    }, [dispatch, userInfo]);
-
-    return (
-        <div>
-            {loading ? (
-                <div>Loading...</div>
-            ) : error ? (
-                <div>{error}</div>
-            ) : (
-                        <>
-                            <div className="content content-margined">
-                                <h3>Pivot Table Stock Events</h3>
-                            </div>
-                            {userInfo && userInfo.user.isTop && (
-
-
-                                <PivotTableUI
-                                    data={stockevents}
-                                    onChange={s => setInitialState(s)}
-                                    {...initialState}
-                                />
-                            )}
-
-                        </>
-                    )}
-        </div>
-    );
-
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <>
+          <div className='content content-margined'>
+            <h3>Pivot Table Stock Events</h3>
+          </div>
+          {userInfo && userInfo.user.isTop && (
+            <PivotTableUI
+              data={stockevents}
+              onChange={s => setInitialState(s)}
+              {...initialState}
+            />
+          )}
+        </>
+      )}
+    </div>
+  )
 }
 
-export default PTableStockEvents;
+export default PTableStockEvents
